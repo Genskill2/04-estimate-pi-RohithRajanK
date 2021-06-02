fc03efbe59e600dbdbb5be172873ac3b335f158c
RohithRajanK <84078186+RohithRajanK@users.noreply.github.com>
2021-06-02 14:42:29 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int count);
+float mc_pi(int);
 
 float frandom() {
   long int q = random();
@@ -38,24 +38,20 @@ int main(void) {
     }
   }
 }
-
-float mc_pi(int n)
+float mc_pi(int count)
 {
-	
-	float cir=0;
-	for(int i=0;i<n;i++)
+	float circount=0;
+	for(int i=0;i<count;i++)
 	{
 		
 		float x = frandom();
 		float y = frandom();
 		if(sqrt(x*x+y*y)<1)
 		{
-			cir++;
+			circount++;
 		}
 	}
-	float area = ((4*cir)/n);
+	float area = ((4*circount)/count);
 	return area;
-
-		
 }
 
